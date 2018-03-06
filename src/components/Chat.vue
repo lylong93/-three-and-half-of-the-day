2afb22c85554cf959d63af4d19a10a17f2655bbf
Yoghurt <695498118@qq.com>
2018-03-06 23:34:10 +0800

    chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,7 +2,7 @@
   <div class="chat">
     <div class="chat-header">
       <span :class="{act:value}">和小智对话</span>
-      <el-switch v-model="value" active-color="#abb88e" inactive-color="#13ce66">
+      <el-switch v-model="value" active-color="#13ce66" inactive-color="#abb88e">
       </el-switch>
     </div>
     <div class="chat-main">
@@ -14,7 +14,7 @@
         </li>
       </ul>
     </div>
-    <div>
+    <div class="chat-footer">
       <el-input class="inp" v-model="sendMsg" placeholder="请输入内容"></el-input>
       <el-button class="but" type="success" @click="sen">发送信息</el-button>
     </div>
@@ -27,7 +27,7 @@ export default {
     return {
       sendMsg: null,
       msgs: [],
-      value: true
+      value: false
     }
   },
   mounted() {
@@ -42,6 +42,9 @@ export default {
       this.sendMsg = null
     },
     sen() {
+      if (!this.sendMsg) {
+        return
+      }
       const that = this
       const msg = {
         user: 'one',
@@ -49,8 +52,8 @@ export default {
       }
       that.msgs.push(msg)
       axios.post('/api', {
-        "key": '425487400d86479bb3c987856bce3bd2',
-        "info": that.sendMsg
+        'key': '425487400d86479bb3c987856bce3bd2',
+        'info': that.sendMsg
       }).then(function(response) {
         const msg = {
           user: 'api',
@@ -66,7 +69,7 @@ export default {
 </script>
 <style lang='scss'>
 .chat-header {
-  height: 50px;
+  height: 25px;
   background: #963;
 }
 
@@ -80,24 +83,24 @@ export default {
   }
 }
 
+.chat-footer {
+  .inp {
+    width: 65%; // height: 50px;
+    position: fixed;
+    bottom: 30px;
+    left: 5px;
+  }
+
+  .but {
+    width: 30%; // height: 50px;
+    position: fixed;
+    bottom: 30px;
+    right: 5px;
+  }
+}
+
 .act {
-  color: red;
-}
-
-.inp {
-  width: 70%;
-  height: 50px;
-  position: fixed;
-  bottom: 50px;
-  left: 0px;
-}
-
-.but {
-  width: 20%;
-  height: 50px;
-  position: fixed;
-  bottom: 50px;
-  right: 0px;
+  color: #6f6;
 }
 
 </style>
